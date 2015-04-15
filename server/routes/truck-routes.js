1a4298411e4375a1490eaeb7e4ab32e8cbb75db4
Ian McCunn <ianmccunn@gmail.com>
2015-04-15 16:19:08 -0700

    converted indentation to spaces

--- server/routes/truck-routes.js
+++ server/routes/truck-routes.js
@@ -6,11 +6,11 @@ var bodyParser = require('body-parser');
 var eatAuth = require('../lib/eat_auth');
 
 module.exports = function(app, appSecret) {
-	app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-	app.get('/trucks/:id', eatAuth(appSecret), function(req, res) {
-			// Locate truck here
-		});
+  app.get('/trucks/:id', eatAuth(appSecret), function(req, res) {
+      // Locate truck here
+    });
 
-	});
+  });
 };
