4e52b4a5a51b5cae9bb9dec6dc5e122c1f09fa8a
Ian McCunn <ianmccunn@gmail.com>
2015-04-15 15:04:59 -0700

    final, initial webpack/gulpfile config

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,15 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var webpack = require('gulp-webpack');
+var wpConfig = require('./webpack.config.js');
 
 // Concatenate Client
-gulp.task('clientjs', function() {
-  return gulp.src('client/**/*.js')
-             .pipe(concat('all.js'))
-             .pipe(gulp.dest('build/'))
-             .pipe(rename('all.min.js'))
+gulp.task('webpack', function() {
+  return gulp.src('./client/**/*.js')
+             .pipe(webpack(wpConfig))
              .pipe(uglify())
-             .pipe(gulp.dest('build/'));
+             .pipe(gulp.dest('./build'));
 });
 
 gulp.task('clienthtml', function() {
