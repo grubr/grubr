f703a8bf5ab07ab063d012690c61594d2d8ee886
Ian McCunn <ianmccunn@gmail.com>
2015-04-15 16:15:48 -0700

    added initial implementation for trucks route and other necessary packages

---
+++ server/routes/truck-routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// ** Insert truck model import here ** (Trucks)
+
+var bodyParser = require('body-parser');
+var eatAuth = require('../lib/eat_auth');
+
+module.exports = function(app, appSecret) {
+	app.use(bodyParser.json());
+
+	app.get('/trucks/:id', eatAuth(appSecret), function(req, res) {
+			// Locate truck here
+		});
+
+	});
+};
