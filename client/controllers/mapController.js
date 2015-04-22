c8b01da66f01f6909693b17161a7a49f85c026e5
Sean Golob <seangolob@live.com>
2015-04-22 08:49:56 -0700

    trying to make filters work

--- client/controllers/mapController.js
+++ client/controllers/mapController.js
@@ -1,24 +1,37 @@
 'use strict';
 
+var testArray = require('../lib/testArray');
+
 module.exports = function(app) {
   app.controller('mapController', ['$scope', '$http', function($scope, $http){
     $scope.test = 'hello';
-    $scope.markers = [];
+    $scope.markers = testArray;
     $scope.markersShown = [];
     $scope.testMarker = new google.maps.Marker({
       position: new google.maps.LatLng(47.620171, -122.337062),
       title: "test marker"
     });
 
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(47.620171, -122.337062),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+    //initialize map with all markers for that area
     $scope.initialize = function() {
-      var mapCanvas = document.getElementById('map-canvas');
-      var mapOptions = {
-        center: new google.maps.LatLng(47.620171, -122.337062),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      var map = new google.maps.Map(mapCanvas, mapOptions);
-      $scope.testMarker.setMap(map);
+
+      for(var i = 0; i < $scope.markers.length; i++) {
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng($scope.markers[i].lat, $scope.markers[i].long),
+          title: $scope.markers[i].name
+        });
+        marker.setMap(map);
+      };
+
     };
 
     google.maps.event.addDomListener(window, 'load', $scope.initialize);
@@ -37,9 +50,25 @@ module.exports = function(app) {
       });
     };
 
-    $scope.filterMarkers = function(filter) {
+    $scope.filterMarkersByType = function(foodType) {
+      setAllMap(null);
 
-    }
+      console.log('hit filter');
+      for (var i = 0; i < $scope.markers.length; i++) {
+        if($scope.markers[i].type == foodType) {
+          $scope.markersShown.push($scope.markers[i]);
+        }
+      }
+
+      for(i = 0; i < $scope.markersShown.length; i++) {
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng($scope.markersShown[i].lat, $scope.markersShown[i].long),
+          title: $scope.markersShown[i].name
+        });
+        marker.setMap(map);
+      };
+
+    };
 
   }]);
-}
+};
---
+++ client/lib/testArray.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = [{
+  id: 1,
+  name: 'Taco Truck',
+  type: 'Mexican',
+  city: 'South Lake Union',
+  vege: true,
+  lat: 47.623356,
+  long: -122.335721
+}, {
+  id: 2,
+  name: 'I love my GFF',
+  type: 'Other',
+  city: 'South Lake Union',
+  vege: true,
+  lat: 47.623255,
+  long: -122.336097
+}, {
+  id: 3,
+  name: 'Another Taco Truck',
+  type: 'Mexican',
+  city: 'South Lake Union',
+  vege: true,
+  lat: 47.623056,
+  long: -122.337266
+}];
+
+
