06e87aba0a6a4a1041f7b3a539387aa833abae7f
Ian McCunn <ianmccunn@gmail.com>
2015-04-28 10:53:46 -0700

    trucks object fix, should be plural

--- server/routes/truck-routes.js
+++ server/routes/truck-routes.js
@@ -9,8 +9,7 @@ module.exports = function(app) {
   app.use(bodyParser.json());
 
   app.get('/trucks', /*eatAuth(appSecret),*/ function(req, res) {
-    var truckID = parseInt(req.id);
-    DB.truck.findAll().then(function(truck) {
+    DB.trucks.findAll().then(function(truck) {
       res.send(truck);
     }).catch(function(error) {
       res.send({'msg': 'Unable to find truck.'});
