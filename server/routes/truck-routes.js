caf195e7dd73e13138aa82f6624041430079dc71
Sean Golob <seangolob@live.com>
2015-05-01 10:17:00 -0700

    update master

--- server/routes/truck-routes.js
+++ server/routes/truck-routes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
   app.use(bodyParser.json());
 
   app.get('/trucks', /*eatAuth(appSecret),*/ function(req, res) {
-    DB.truck.findAll().then(function(truck) {
+    DB.trucks.findAll().then(function(truck) {
       res.send(truck);
     }).catch(function(error) {
       res.send({'msg': 'Unable to find truck.'});
